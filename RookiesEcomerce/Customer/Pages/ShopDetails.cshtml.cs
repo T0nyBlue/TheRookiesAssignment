03ce5e0e17ada44021b11e60f0d26954119be2e3
T0nyBlue <18521123@gm.uit.edu.vn>
2022-03-12 08:03:04 +0700

    Create Shop Details

---
+++ RookiesEcomerce/Customer/Pages/ShopDetails.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Customer.Pages
+{
+    public class ShopDetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
