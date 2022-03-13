4a1dafcbf2f0ba8384bc53a3945242b89b3272e1
T0nyBlue <18521123@gm.uit.edu.vn>
2022-03-13 11:18:18 +0700

    Update Shop Details

--- RookiesEcomerce/Customer/Pages/ShopDetails.cshtml.cs
+++ RookiesEcomerce/Customer/Pages/ShopDetails.cshtml.cs
@@ -1,3 +1,7 @@
+using AutoMapper;
+using DataAccess.Data;
+using DataAccess.DTO.ProductDto;
+using DataAccess.DTO.ProductImgDto;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +9,26 @@ namespace Customer.Pages
 {
     public class ShopDetailsModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _db;
+        private readonly IMapper _mapper;
+
+        [BindProperty(SupportsGet = true, Name = "id")]
+        public string Id { get; set; }
+
+        public ProductReadDto productDtoResponse { get; set; }
+
+        public ShopDetailsModel(ApplicationDbContext db, IMapper mapper)
         {
+            _db = db;
+            _mapper = mapper;
+        }
+
+        public async void OnGet()
+        {
+            var productResponse = _db.Products.FirstOrDefault(x => x.ProductId.ToString() == Id);
+
+            productDtoResponse = _mapper.Map<ProductReadDto>(productResponse);
+            productDtoResponse.ProductImgReadDto = _mapper.Map<List<ProductImgReadDto>>(_db.ProductImgs.Where(x => x.ProductId == productDtoResponse.ProductId));
         }
     }
 }
